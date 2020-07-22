eea7883d80880cfdbf17e355b60169ab014bdd47
Joseph Yeh <josephycyeh@gmail.com>
2020-07-22 02:51:23 -0700

    nasdn

--- src/components/Column.jsx
+++ src/components/Column.jsx
@@ -30,23 +30,21 @@ function Column(prop) {
         style={{background: prop.snapshot.isDraggingOver ? '#F3F4F6' : '#F3F4F6', padding: 4, width: 250, minHeight: 500}}
         >
             {prop.column.items.map((item, index) => {
-                                            return (
-                                                <Draggable key={item.id} draggableId={item.id} index={index}>
-                                                    {(provided, snapshot) => {
-                                                        return (
-                                                            <Task provided={provided} snapshot= {snapshot} text = {item.content} img={item.assigned} date={item.date} color={item.color} />
-
-                                                            
-                                                        );
-                                                    }}
-                                                </Draggable>
-                                            );
-                                        })}
-                                    {prop.provided.placeholder}
+                    return (
+                        <Draggable key={item.id} draggableId={item.id} index={index}>
+                            {(provided, snapshot) => {
+                                return (
+                                    <Task provided={provided} snapshot= {snapshot} text = {item.content} img={item.assigned} date={item.date} color={item.color} />             
+                                         );
+                                    }}
+                         </Draggable>
+                        );
+                    })}
+                {prop.provided.placeholder}
         </div>
-                        {clicked ?  
-                        <AddScreen toggleAdd={toggleAdd} addTask={addTask} id={prop.id} />  
-                     : null } 
+            {clicked ?  
+            <AddScreen toggleAdd={toggleAdd} addTask={addTask} id={prop.id} />  
+             : null } 
         </div>
     )
 }
