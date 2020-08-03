f51772117e4f3b23b602bd239ac759ad59255df5
Joseph Yeh <josephycyeh@gmail.com>
2020-08-02 23:07:19 -0700

    ye

--- src/App.js
+++ src/App.js
@@ -79,7 +79,7 @@ function App() {
         }
         axios.get("/exercises")
         .then(response => {
-            
+            console.log(reponse.data)
             if (response.data.length > 0) {
                  response.data.forEach((task) => {
                     col[task.id] = {
